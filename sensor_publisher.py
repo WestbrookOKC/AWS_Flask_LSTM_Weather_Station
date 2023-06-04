3a49796bd50e5d5b5724129f097eb98d99a883cc
WestbrookOKC <100655843+WestbrookOKC@users.noreply.github.com>
2023-06-04 13:11:43 +0800

    Update sensor_publisher.py

--- sensor_publisher.py
+++ sensor_publisher.py
@@ -5,7 +5,7 @@ import json
 import datetime
 # configure the credentials for cilent
 client = AWSIoTMQTTClient("raspberry")
-client.configureEndpoint("a1ezbd0l3p8fzz-ats.iot.us-east-2.amazonaws.com", 8883)
+client.configureEndpoint("xxx", 8883)
 client.configureCredentials("./root-CA.crt", "./Raspberry.private.key", "./Raspberry.cert.pem")
 
 # Connect to AWS IoT
